62d5727b53882d0c2b7f1d345de804c115e99582
davemollen <davemollen@gmail.com>
2019-06-04 15:03:15 +0200

    add socketIO

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express=require("express")
+const socketIo = require('socket.io')
 const bodyParser = require('body-parser')
 const app = express()
 var cors = require('cors')
@@ -9,4 +10,14 @@ app
 .use(bodyParser.json())
 .use(cors())
 .use(authRouters)
-.listen(port, () => console.log(`Listening on port ${port}`))
+
+const server = app.listen(port, () => console.log(`Listening on port ${port}`))
+const io = socketIo.listen(server)
+
+io.on('connection', client => {
+  console.log('client.id test: ', client.id)
+
+  client.on('disconnect', () => {
+    console.log('disconnect test:', client.id)
+  })
+})
