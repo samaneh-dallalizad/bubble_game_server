96255528d196107442ebaae6138cd908324edd95
davemollen <davemollen@gmail.com>
2019-06-04 17:35:02 +0200

    got something working

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express=require("express")
 const socketIo = require('socket.io')
 const bodyParser = require('body-parser')
 const app = express()
-var cors = require('cors')
+const cors = require('cors')
 const authRouters = require('./auth/route')
 const dispatcher = require('./game/dispatcher')
 const routing = require('./game/routing')
+const gameDummyDatabase = require('./game/gameDummyDatabase')
 const port = process.env.PORT || 4000
 
 app
@@ -17,13 +18,14 @@ const server = app.listen(port, () => console.log(`Listening on port ${port}`))
 const io = socketIo.listen(server)
 
 // gameRouter
-const bubbles = ['blue', 'red', 'green'];
 const dispatch = dispatcher(io)
-const router = routing(dispatch, bubbles)
+const router = routing(dispatch, gameDummyDatabase)
 app.use(router)
 
 io.on('connection', client => {
   console.log('client.id test: ', client.id)
+
+  dispatch(gameDummyDatabase)
 
   client.on('disconnect', () => {
     console.log('disconnect test:', client.id)
